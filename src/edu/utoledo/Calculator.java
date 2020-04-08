ce8399b53a67b935fd1da54bfdad5b6cfcb03c69
donmiller <don@groundspeedhq.com>
2020-04-08 11:21:29 -0400

    Added calculator class

---
+++ src/edu/utoledo/Calculator.java
@@ -0,0 +1,8 @@
+package edu.utoledo;
+
+public class Calculator {
+
+    public int AddTwoNumbers(int num1, int num2) {
+        return num1 + num2;
+    }
+}
--- src/edu/utoledo/Main.java
+++ src/edu/utoledo/Main.java
@@ -3,6 +3,6 @@ package edu.utoledo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Goodbye World!");
+        System.out.print("2 + 2 = " + new Calculator().AddTwoNumbers(2, 2));
     }
 }
